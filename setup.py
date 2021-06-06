894751051a1cc04a75ee1b8e6fbf97e46fe8080e
Jesper Schmitz Mouridsen <jesper@schmitz.computer>
2021-06-06 22:26:12 +0200

    Update setup.py
    Update django-tables2

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
                       "django-ranged-response==0.2.0",
                       "django-session-security==2.6.6",
                       "django-simple-captcha==0.5.12",
-                      "django-tables2==2.2.1",
+                      "django-tables2==2.4.0",
                       "humanize==0.5.1",
                       "rules==2.2"
                       ]
